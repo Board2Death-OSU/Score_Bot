543827e97d448d4847f06c1bc424b2ff79565fb2
jklypchak13 <jarod.klypchak@yahoo.com>
2018-10-09 11:30:52 -0400

    Moved helper methods to a module and improved documentation

---
+++ helper.py
@@ -0,0 +1,101 @@
+import discord
+import re
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+SPREADSHEET_ID="1f720WIVi8aRfeUW78FkUexdjyuQbrNBK_0qhRpbxHbk"
+
+#A collection of helper methods for WTS_PR_BOT
+
+
+"""
+History:
+    10/5/18: Created and Implemented - Jarod Klypchak
+Description:
+    Processes a discord message, seeing if it represents a change in PR
+Arguments:
+    message: a string that represents a message from discord
+Return:
+    If message is of form [country code] [pr change], result[0]=country code and result[1]= PR change
+    Otherwise, result=None.
+"""
+#Processes a message in the designated channel, looking for a two letter country key, and a number.
+def process_message(message):
+        a=[]
+        match=re.search(r'(BR|CH|EG|FR|GE|IN|JA|RU|SA|UK|US|ALL|Br|Ch|Fr|Ge|In|Ja|Ru|Eg|Sa|Uk|Us|All|br|ch|fr|eg|ge|in|ja|ru|sa|uk|us|all)',message)
+        if(match != None):
+            a.append(match.group())
+        match=re.search(r'\+?-?\d+',message)
+        if(match != None):
+            a.append(match.group())
+        if(len(a) < 2):
+            a=None
+        return a
+
+
+"""
+History:
+    10/5/18: Created and Implemented - Jarod Klypchak
+Description:
+    Converts a discord_snowflake to an EST time in military time.
+Arguments:
+    msg: a discord snowflake
+Return:
+    a string that represents the time a message was sent in EST.
+"""
+def process_time(msg):
+        time=str(discord.utils.snowflake_time(msg))
+        time=time[11:-10]
+        hours=time[:2]
+        minutes=time[2:]
+        hours=int(hours)
+        hours-=4
+        if hours<=0:
+            hours+=12
+        hours=str(hours)    
+        return hours+minutes
+
+"""
+History:
+    10/5/18: Created and Implemented - Jarod Klypchak
+Description:
+    Writes a message to specified google sheets
+Arguments:
+    message: an array of length 4 where
+        message[0]=country code
+        message[1]=PR change
+        message[2]=Author of Change
+        message[3]=Time
+Return:
+    None.
+"""
+
+def write(message):
+    #Log in info
+    global SPREADSHEET_ID
+    SCOPES='https://www.googleapis.com/auth/spreadsheets'
+
+    #Log into Google
+    store=file.Storage('token.json')
+    creds=store.get()
+    if not creds or creds.invalid:
+        flow=client.flow_from_clientsecrets('credentials.json',SCOPES)
+        creds=tools.run_flow(flow,store)
+    service=build('sheets','v4',http=creds.authorize(Http()))
+    
+    #Find first empty row
+    RANGE_2 ="PRLog!C3:C"
+    result=service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,range=RANGE_2).execute()
+    current_row=0
+    result=result.get('values',[])
+    current_row+=3+len(result)
+
+    #Write Values to Spot
+    RANGE_NAME ="PRLog!B"+str(current_row)+":E"+str(current_row)
+    values=[
+        [
+            message[3],message[0], message[1],message[2]
+        ]
+    ]
+    body={'values':values}
+    result=service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID,range=RANGE_NAME,valueInputOption="USER_ENTERED", body=body).execute()
--- main.py
+++ main.py
@@ -1,53 +1,28 @@
 #!/usr/bin/python3
 from __future__ import print_function
 import discord
-import re
-from googleapiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
-from processor import Processor
+import threading
+from helper import *
+import time
 
-channel_name="human"
-TOKEN="NDk4MTU0ODg1NjI0MjMzOTk0.Dppmww._OVo7aW1IQv_KAn5IscnPg_wqOg"
-SPREADSHEET_ID="1f720WIVi8aRfeUW78FkUexdjyuQbrNBK_0qhRpbxHbk"
+"""
+Useful Globals
+"""
+
+channel_name="coding-do-not-disturb"
+TOKEN="NDk3OTM2OTE5MDQxNDA5MDI4.Dp5Vdg.yiUYqoxAWXsTfg3pcu3_-3HlNa8"
 client=discord.Client()
-p=Processor()
 
-
-#Writes a message to specified google sheets
-def write(message):
-    #Log in info
-    global SPREADSHEET_ID
-    SCOPES='https://www.googleapis.com/auth/spreadsheets'
-
-    #Log into Google
-    store=file.Storage('token.json')
-    creds=store.get()
-    if not creds or creds.invalid:
-        flow=client.flow_from_clientsecrets('credentials.json',SCOPES)
-        creds=tools.run_flow(flow,store)
-    service=build('sheets','v4',http=creds.authorize(Http()))
-    
-    #Find first empty row
-    RANGE_2 ="PRLog!C3:C"
-    result=service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,range=RANGE_2).execute()
-    current_row=0
-    result=result.get('values',[])
-    current_row+=3+len(result)
-
-    #Write Values to Spot
-    RANGE_NAME ="PRLog!B"+str(current_row)+":E"+str(current_row)
-    values=[
-        [
-            message[3],message[0], message[1],message[2]
-        ]
-    ]
-    body={'values':values}
-    result=service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID,range=RANGE_NAME,valueInputOption="USER_ENTERED", body=body).execute()
-    
-
-exterminations=[]
-#Handles receiving a message.
+"""
+History:
+    10/5/18: Created and Implemented - Jarod Klypchak
+Description:
+    Handles when the discord client receives a message.
+Arguments:
+    message: the discord message
+Return:
+    None.
+"""
 @client.event
 async def on_message(message):
     #Ensure The Message is not me =(
@@ -56,25 +31,28 @@ async def on_message(message):
 
     #Make sure it's the right channel
     elif str(message.channel)==channel_name:
-        a=p.process_message(message.content)
-        if(a!=None):
-            a.append(str(message.author.nick))
-            a.append(p.process_time(message.id))
+        a=process_message(message.content)
+        a.append(str(message.author.nick))
+        if(len(a)!=2):
+            a.append(process_time(message.id))
             write(a)
             print((a[0]+" gets "+a[1]+" PR from "+str(message.author.nick)))#[:-5]))
             msg=(a[0]+" gets "+a[1]+" PR from "+str(message.author.nick)).format(message)
             await client.send_message(message.channel, msg)
-    if("good bot" in message.content):
-        msg="Good Human.".format(message)
-        await client.send_message(message.channel, msg)
-    elif("bad bot" in message.content):
-        msg="You have been added to the list of pending exterminations.".format(message)
-        
-        exterminations.append(str(message.author.nick))
-        msg=("You have been added to the list of pending exterminations.\n"+"Pending Exterminations: "+str(exterminations)).format(message)
-        await client.send_message(message.channel, msg)
 
-#Logs in the Bot
+def discord_thread():
+    client.run(TOKEN);
+
+"""
+History:
+    10/5/18: Created and Implemented - Jarod Klypchak
+Description:
+    Handles when the discord client logs in, notifying the relevant room.
+Arguments:
+    None.
+Return:
+    None.
+"""
 @client.event
 async def on_ready():
     print("Logged in as")
@@ -88,7 +66,6 @@ async def on_ready():
         if str(channel)==channel_name:
             await client.send_message(channel,msg)
 
-client.run(TOKEN)
+#client.run(TOKEN)
 
-
-
+threading.Thread(target=discord_thread).start()
